adacc87614a61fdb3a7eaa73be8fd62c0ab0e0d9
Chun Ly <astro.chun@gmail.com>
2018-03-01 23:13:50 -0700

    subsample: Add summary_table function

--- subsample.py
+++ subsample.py
@@ -82,6 +82,75 @@ def galaxy_field(tab0, field, silent=False, verbose=True):
     return g_field0
 #enddef
 
+def summary_table(sub_dict0, gal_field, gal_field0=None, silent=False,
+                  verbose=True):
+    '''
+    Generate Astropy table of number of subsamples for each galaxy field
+
+    Parameters
+    ----------
+    sub_dict0 : dict
+      Dictionary containing indices for each NB excess emitter subsample.
+      From main()
+
+    gal_field : list
+      List of string containing the field name [from galaxy_field()]
+
+    silent : boolean
+      Turns off stdout messages. Default: False
+
+    verbose : boolean
+      Turns on additional stdout messages. Default: True
+
+    Returns
+    -------
+    tab0 : astropy.table.table.Table
+      Table of number of excess emitters for each subsample and galaxy field
+
+    Notes
+    -----
+    Created by Chun Ly, 1 March 2018
+    '''
+
+    if type(gal_field0) == type(None):
+        gal_field0 = list(set(gal_field))
+
+    n_fields = len(gal_field0)
+
+    fieldname   = np.array(gal_field0)
+    n_HaNB816   = np.zeros(n_fields, dtype=np.int16)
+    n_OIIINB816 = np.zeros(n_fields, dtype=np.int16)
+    n_OIINB816  = np.zeros(n_fields, dtype=np.int16)
+
+    n_HaNB921   = np.zeros(n_fields, dtype=np.int16)
+    n_OIIINB921 = np.zeros(n_fields, dtype=np.int16)
+    n_OIINB921  = np.zeros(n_fields, dtype=np.int16)
+
+    for ff in range(n_fields):
+        f_idx = [xx for xx in range(len(gal_field)) if
+                 gal_field[xx] == gal_field0[ff]]
+
+        for key in sub_dict0.keys():
+            s_idx = sub_dict0[key]
+
+            # Get the intersection of f_idx and s_idx
+            t_idx = list(set(f_idx) & set(s_idx))
+
+            cmd = 'n_%s[%i] = %i ' % (key.replace('_NB0','NB'), ff, len(t_idx))
+            exec(cmd)
+        #endfor
+    #endfor
+
+    arr0 = [fieldname, n_HaNB816, n_OIIINB816, n_OIINB816,
+            n_HaNB921, n_OIIINB921, n_OIINB921]
+    names0 = ['Field', 'HaNB816', 'OIIINB816', 'OIINB816',
+              'HaNB921', 'OIIINB921', 'OIINB921']
+    tab0 = Table(arr0, names=names0)
+
+    if silent == False: tab0.pprint()
+    return tab0
+#enddef
+
 def main(tab0=None, field='', dr='pdr1', silent=False, verbose=True):
 
     '''
