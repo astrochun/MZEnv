9110bcbde15c8f747d4e517733298e331e95794e
Chun Ly <astro.chun@gmail.com>
2018-03-21 13:40:55 -0700

    plotting.ra_dec_distribution: Add plot_bino_fov function

--- plotting/ra_dec_distribution.py
+++ plotting/ra_dec_distribution.py
@@ -179,6 +179,98 @@ def plot_deimos_fov(ax, coord, maskno, pa=0.0):
     return ax, verts0 # Mod on 03/03/2018
 #enddef
 
+def plot_bino_fov(ax, coord, maskno, pa=0.0):
+    '''
+    Overlay MMT/Binospec FoV on image
+
+    Parameters
+    ----------
+    ax : matplotlib.axes._subplots.AxesSubplot
+      Matplotlib axes from plt.subplots()
+
+    coord : list of list
+      Central RA,Dec coordinate provided in degrees. Each position is provided
+      as a list within the main list.
+      coord = [ [RA1,Dec1], [RA2,Dec2], ...]
+
+    maskno : list
+      List of strings for mask name shorthand to overlay on top of mask
+
+    pa : list
+      List of PA in degrees for DEIMOS orientation. Positive is E of North.
+      Default: 0.0 deg
+
+    silent : boolean
+      Turns off stdout messages. Default: False
+
+    verbose : boolean
+      Turns on additional stdout messages. Default: True
+
+    Returns
+    -------
+     ax with Rectangular patch added
+
+     verts0 : list
+       List containing vertices for each Rectangle patch
+
+    Notes
+    -----
+    Created by Chun Ly, 21 March 2018
+    '''
+
+    dx0 = 19.2 / 60.0 # for outside; in deg
+    dx1 =  3.2 / 60.0 # for inside; in deg
+    dy0 = 15.0 / 60.0 # in deg
+
+    xbox0 = np.array([ 1, 1,-1,-1])*dx0/2.0
+    xbox1 = np.array([ 1, 1,-1,-1])*dx1/2.0
+    ybox0 = np.array([-1, 1, 1,-1])*dy0/2.0
+
+    n_ptgs = len(coord)
+
+    verts0 = []
+
+    for cc in range(n_ptgs):
+        rad = -1*pa[cc]*np.pi/180.0 #Sign flip
+
+        # outside
+        xprime0 =  xbox0*np.cos(rad) - ybox0*np.sin(rad) #Sign fix
+        yprime0 =  xbox0*np.sin(rad) + ybox0*np.cos(rad) #Sign fix
+
+        # inside
+        xprime1 =  xbox1*np.cos(rad) - ybox0*np.sin(rad) #Sign fix
+        yprime1 =  xbox1*np.sin(rad) + ybox0*np.cos(rad) #Sign fix
+
+        # side 1
+        xbox1 = coord[cc][0] + [xprime0[0:2], xprime1[2:]]
+        ybox1 = coord[cc][1] + [yprime0[0:2], yprime1[2:]]
+
+        # side 2
+        xbox2 = coord[cc][0] + [xprime1[0:2], xprime0[2:]]
+        ybox2 = coord[cc][1] + [yprime1[0:2], yprime0[2:]]
+
+        xy_low1 = [xbox1[3], ybox1[3]]
+        xy_low2 = [xbox2[3], ybox2[3]]
+
+        t_rot0 = -90+pa[cc] if pa[cc] > 0 else 90+pa[cc]
+        ax.text(coord[cc][0], coord[cc][1], maskno[cc], ha='center', va='center',
+                rotation=t_rot0)
+
+        rect1 = mpatches.Rectangle(xy_low1, dx1, dy0, angle=-1*pa[cc],
+                                  linewidth=1.5, ec="k", color="none")
+        rect2 = mpatches.Rectangle(xy_low2, dx1, dy0, angle=-1*pa[cc],
+                                   linewidth=1.5, ec="k", color="none")
+
+        # Must be done before ax.add_patch call
+        verts0.append([rect1.get_verts(), rect2.get_verts()])
+        ax.add_patch(rect1)
+        ax.add_patch(rect2)
+    #endfor
+
+    return ax, verts0 # Mod on 03/03/2018
+
+#enddef
+
 def in_hecto_field(tab0, fld_coord, silent=False, verbose=True):
     '''
     Determine sources in MMT/Hectospec field
